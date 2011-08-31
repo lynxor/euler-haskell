76d51efef8a4640b93cf2f2b91af7ad4539bf801
Dawid Malan <dawidm@discovery.co.za>
2011-08-31 10:11:34 +0200

    cleaned up a bit

--- problem4.hs
+++ problem4.hs
@@ -1,10 +1,6 @@
-problem4 a = head $ dropWhile (dropFunc [a,a-1 .. a-(a `div` 10) ]) (findPalindromes a)
+-- Call like this : problem4 999, or problem4 99
+problem4 a = head $ dropWhile (dropFunc [a,a-1 .. a-(a `div` 10) ]) (findPalindromes)
   where dropFunc options item = null $ dropWhile (\p -> item `mod` p /= 0 || not (elem (item `div` p)  options)) options 
-        
+        findPalindromes = filter (palindrome.show) [a*a, a*a -1 .. ]
+        palindrome x = reverse x == x
 
-findPalindromes :: (Integral a) => a -> [a]
-findPalindromes x = filter (palindrome.show) [x*x, x*x - 1 .. ]   
-
-        
-palindrome :: String -> Bool
-palindrome a = reverse a == a
