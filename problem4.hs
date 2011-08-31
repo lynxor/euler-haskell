fecc4c4a64a313a1ba115f3d6120a50a1587ad4e
Dawid Malan <dawidm@discovery.co.za>
2011-08-31 10:07:30 +0200

    problem4 done!

--- problem4.hs
+++ problem4.hs
@@ -1,5 +1,10 @@
-biggestPal :: (Integral a) => a -> String
-biggestPal a = if palindrome (read a) then show a else biggestPal 
-  where palindrome a = reverse a == a
-        biggest x y = isPalindrome read (x * y)
+problem4 a = head $ dropWhile (dropFunc [a,a-1 .. a-(a `div` 10) ]) (findPalindromes a)
+  where dropFunc options item = null $ dropWhile (\p -> item `mod` p /= 0 || not (elem (item `div` p)  options)) options 
         
+
+findPalindromes :: (Integral a) => a -> [a]
+findPalindromes x = filter (palindrome.show) [x*x, x*x - 1 .. ]   
+
+        
+palindrome :: String -> Bool
+palindrome a = reverse a == a
